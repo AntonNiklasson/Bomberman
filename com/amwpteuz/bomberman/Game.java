e65f0a776caa4ae9fe61e2b8662cf7c1d7d6bc04
Fnatte <matteus92@gmail.com>
2011-12-05 21:12:29 +0100

    Adding core classes such as Main and Game. But also a simple window with a grid class.

---
+++ com/amwpteuz/bomberman/Game.java
@@ -0,0 +1,34 @@
+package com.amwpteuz.bomberman;
+
+/**
+ * Core/root class. Encapsulates all components of the game. 
+ * 
+ * @author Matteus
+ *
+ */
+public class Game extends Thread {
+	
+	private GameWindow window;
+	private boolean isRunning;
+	
+	public Game() {
+		window = new GameWindow();
+	}
+	
+	@Override
+	public void run() {
+		super.run();
+		
+		Log.get().info("Game running");
+		while(isRunning) {
+			
+		}
+		
+	}
+	
+	@Override
+	public synchronized void start() {
+		super.start();
+		Log.get().info("Game started");
+	}
+}
