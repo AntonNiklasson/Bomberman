fd0a5222b1308e824b1f1d00fdee0d44a08744b1
Fnatte <matteus92@gmail.com>
2011-12-10 03:10:02 +0100

    Enemies can no longer spawn trapped. Fixed a bug when enemy spawned in a corner.

---
+++ src/com/anwpteuz/bomberman/Direction.java
@@ -0,0 +1,44 @@
+package com.anwpteuz.bomberman;
+
+public enum Direction {
+	
+	LEFT(-1, 0),
+	RIGHT(1, 0),
+	UP(0, -1),
+	DOWN(0, 1);
+	
+	private final int x, y;
+	public static final Direction[] directions = new Direction[] {
+		Direction.LEFT,
+		Direction.RIGHT,
+		Direction.UP,
+		Direction.DOWN
+	};
+	
+	Direction(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+	
+	/**
+	 * 
+	 * @return Returns the inverse to the direction.
+	 */
+	public Direction getInverse() {
+		switch(this) {
+		case LEFT: return RIGHT;
+		case RIGHT: return LEFT;
+		case UP: return DOWN;
+		case DOWN: return UP;
+		}
+		return null;
+	}
+}
